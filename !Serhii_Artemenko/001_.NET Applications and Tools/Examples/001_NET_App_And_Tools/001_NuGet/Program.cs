abc004c1454f86ec131edcb4730d110f0f206d2f
streetsata <streetsata@live.ru>
2018-09-20 16:12:05 +0300

    1 lesson was added

---
+++ !Serhii_Artemenko/001_.NET Applications and Tools/Examples/001_NET_App_And_Tools/001_NuGet/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_NuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new XLWorkbook();
+        }
+    }
+}
---
+++ !Serhii_Artemenko/001_.NET Applications and Tools/Readme.md
@@ -0,0 +1,12 @@
+- Обзор истории .NET
+- Разница между .NET Framework и .NET Core
+- NuGet пакеты
+- CLR
+- Особенности Windows Runtime
+- Hello World!
+- .NET Core Command-Line Interface
+- Visual Studio 2017
+- Universal Windows Platform
+- Технологии для создания Windows apps
+- Технологии для создания Web apps
+- GIT
