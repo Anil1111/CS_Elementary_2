43a2b4ccb2bee8a6487487054fa5e81135d542bb
Yakimenko Igor <igor.jakimenko@privatbank.ua>
2018-09-26 23:38:08 +0300

    HomerWork2 Order and Worker

---
+++ IgorYakimenko/HomeWork2/WorkClass/WorkClass/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkClass
+{
+    class Order
+    {
+        public Int16 AccPayer { get; set; }
+        public string NamePayer { get; set; }
+        public string AddressPayer { get; set; }
+        public Int16 InnPayer { get; set; }
+        public double SummPayer { get; set; }
+        public Int16 AccRecipient { get; set; }        
+    }
+}
