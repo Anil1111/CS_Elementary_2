5b858b06e851c24749dd52f23884bf1db270fe21
Podpryadok_Igor <podpryadok@gmail.com>
2018-09-27 16:26:47 +0300

    Not working funkcionality

--- Podpryadok_Igor/HW_Lecture2/home_work_lecture2/home_work_lecture2/Program.cs
+++ Podpryadok_Igor/HW_Lecture2/home_work_lecture2/home_work_lecture2/Program.cs
@@ -9,48 +9,32 @@ namespace home_work_lecture2
     {
         static void Main(string[] args)
         {
+            Worker worker1 = new Worker();
+            worker1.zapisWorker();
+
+            Worker[] workers = new Worker[] { worker1};
+            Array.Sort(workers);
+            foreach(Worker w in workers)
+            {
+                Console.WriteLine(w.nameFO);
+            }
 
             Order order1 = new Order();
             order1.zapisDannih();
-            //foreach(object obj in order1.zapis)
-            //{
-            //    Console.WriteLine(obj);
-            //}
+
             Order order2 = new Order();
             order2.zapisDannih();
             Console.WriteLine();//Dlya lychshei chitaemosti v konsoly
 
             Order[] orders = new Order[] { order1, order2};
             Array.Sort(orders);
-
             foreach (Order o in orders)
             {
-                Console.WriteLine("{0}", o.zapis);
+                Console.WriteLine(o.zapis);
             }
 
-            Console.WriteLine("Введите сумму для сравнения:");
-            int summaDlyaVivoda = Convert.ToInt32(Console.ReadLine());
-
-            for(int i=0; i>orders.Length;++i)
-            {
-                if(order1.summaPerevoda > summaDlyaVivoda)
-                {
-                    
-                    if(order2.summaPerevoda < summaDlyaVivoda)
-                    {
-
-                    }
-                }
-                else
-                {
-                    foreach (Order o in orders)
-                    {
-                        Console.WriteLine(o);
-                    }
-                }
-            }
-
-
+            
+            
             Console.ReadLine();
         }
     }
