be41449f2e639b16f31f25fcdbc77f7de06a8942
Vladimir Tretyak <vladitretyak@i.ua>
2018-10-03 00:02:56 +0300

    Task5HW

---
+++ Vladimir_Tretyak/HomeWork/Task5/WriteTolibrary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task5
+{
+    class WriteTolibrary
+    {
+        string Title;
+        string Author;
+        string Content;
+        Books library = new Books();
+        public void write()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите название новой книги : ");
+                Title = Console.ReadLine();
+                Console.WriteLine("Введите название автора : ");
+                Author = Console.ReadLine();
+                Console.WriteLine("Введите содержание книги : ");
+                Content = Console.ReadLine();
+                Book newBook = new Book(Title,Author,Content);
+                library.inputUserData(newBook);
+                Console.WriteLine("Для окончания ввода нажмите Esc, для продолжения Enter");
+            }
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.Clear();                     
+        }
+        public void Show()
+        {
+            foreach (var b in library.books)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+                Console.WriteLine($"Название книги - {b.Title}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Автор книги - {b.Author}");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Содержание книги - {b.Content}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
