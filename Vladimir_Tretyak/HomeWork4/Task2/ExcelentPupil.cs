9a40d255eef4fa50c50fa5414f0ab82716b34583
Vladimir Tretyak <vladitretyak@i.ua>
2018-10-04 00:07:55 +0300

    HW4T2

---
+++ Vladimir_Tretyak/HomeWork4/Task2/ExcelentPupil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace HW4Task2
+{
+    public class ExcelentPupil : Pupil
+    {
+        public override void Read()
+        {
+            Console.WriteLine("Читает лучше всех,");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("Очень мало отдыхает,");
+        }
+
+        public override void Study()
+        {
+            Console.WriteLine("Отличник,");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("Пишет без ошибок.");
+        }
+
+        public override void print()
+        {          
+            Read();
+            Relax();
+            Study();
+            Write();
+        }
+    }
+}
