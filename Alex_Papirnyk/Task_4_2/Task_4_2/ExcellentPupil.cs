3906f6fe3b2ba95010ea078bcf38076ef67dfd6b
Alexander Papirnyk <gennerys1515@gmail.com>
2018-10-07 19:21:37 +0300

    Task 4 was added

---
+++ Alex_Papirnyk/Task_4_2/Task_4_2/ExcellentPupil.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_2
+{
+    class ExcellentPupil:Pupil
+    {
+        private readonly string marks = String.Empty;
+        public ExcellentPupil()
+        {
+
+        }
+        public ExcellentPupil(string marks, string name, string surname) : base(name, surname)
+        {
+            this.marks = "Bad Performance";
+
+        }
+        public override void Study()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Perfect!Keep it up!");
+            Console.ResetColor();
+        }
+        public override void Read()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Perfect!Keep it up!");
+            Console.ResetColor();
+        }
+        public override void Relax()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Take your time");
+            Console.ResetColor();
+        }
+        public override void Write()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Perfect!Keep it up!");
+            Console.ResetColor();
+        }
+        public override void Print()
+        {
+            ExcellentPupil ep = new ExcellentPupil();
+            ep.Study();
+            ep.Read();
+            ep.Relax();
+            ep.Write();
+        }
+    }
+}
