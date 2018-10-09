af7c864d9297feb799fe3ff89dd1bf027934f2c0
Lena Samoylenko <aknelyomas@gmail.com>
2018-10-09 23:43:06 +0300

    Hometask 3 in process

---
+++ LenaSamoylenko/Hometask3/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time myDateTime = Program.Inizialization();
+            helpClass.Print(myDateTime.DataTime);
+
+            myDateTime.addDateTimeHour = 1;
+            myDateTime.addDateTimeMinute = 1;
+            myDateTime.addDateTimeSeconds = 1;
+            helpClass.Print(myDateTime.DataTime);
+
+            Console.ReadKey();
+        }
+
+        public static Time Inizialization()
+        {
+            Time myDateTime;
+            int year = helpClass.inputData("Put the year please");
+            int month = helpClass.inputData("Put the month please");
+            int day = helpClass.inputData("Put the day please");
+
+            int hours = helpClass.inputData("Put the hours please");
+            int minutes = helpClass.inputData("Put the minutes please");
+            int seconds = helpClass.inputData("Put the seconds please");
+
+            try
+            {
+                myDateTime = new Time(year, month, day, hours, minutes, seconds);
+
+            }
+            catch
+            {
+                myDateTime = null;
+                Console.WriteLine("Some error");
+                Environment.Exit(0);
+            }
+            return myDateTime;
+
+        }
+    }
+}
---
+++ LenaSamoylenko/Hometask3/Task4/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Product: InternetStore
+    {
+        private string productName;
+        private double price;
+        private long barcode;
+
+        public string ProductName => productName;
+        public double Price => price;
+        public long Barcode=> barcode;
+        protected Product() { }
+        protected Product(string productName, double price, long barcode)
+        {
+            this.productName = productName;
+            this.price = price;
+            this.barcode = barcode;
+        }
+    }
+}
