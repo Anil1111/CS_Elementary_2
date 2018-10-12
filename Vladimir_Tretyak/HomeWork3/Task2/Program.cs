da2d82874bb525095c2c47bb4b8e79d5bc23ec9e
Vladimir Tretyak <vladitretyak@i.ua>
2018-10-12 11:26:29 +0300

    HW3T2

--- Vladimir_Tretyak/HomeWork3/Task2/Program.cs
+++ Vladimir_Tretyak/HomeWork3/Task2/Program.cs
@@ -8,19 +8,11 @@ using System.Threading.Tasks;
 
 namespace HW3Task2
 {
-    //2 Класс Абонент: Идентификационный номер, Фамилия, Имя, Отчество, Адрес, Номер кредитной карточки, Дебет, Кредит, 
-    //    Время междугородных и городских переговоров;
-    //    Конструктор; Методы: установка значений атрибутов, получение значений атрибутов, вывод информации.
-    //    Создать массив объектов данного класса. 
-    //    Вывести сведения относительно абонентов,
-    //    у которых время городских переговоров превышает заданное.
-    //    Сведения относительно абонентов, которые пользовались междугородной связью. Список абонентов в алфавитном порядке.
-
     namespace HW3Task2
     {
         class Abonent
         {
-            private long idNumber;
+            long idNumber;
             string lastName;
             string firstName;
             string midleName;
@@ -31,7 +23,6 @@ namespace HW3Task2
             int intercityTalkTimeHours;
             int intercityTalkTimeMin;
             int intercityTalkTimeSec;
-
             int cityTalkTimeHours;
             int cityTalkTimeMin;
             int cityTalkTimeSec;
@@ -50,7 +41,7 @@ namespace HW3Task2
             public int CityTalkTimeHours { get; set; }
             public int CityTalkTimeMin { get; set; }
             public int CityTalkTimeSec { get; set; }
-
+         
             public Abonent(long idNumber, string lastName, string firstName, string midleName, string address, long cardNumber,
                 int cardDebet, int cardCredit, int intercityTalkTimeHours, int intercityTalkTimeMin, int intercityTalkTimeSec,
                 int cityTalkTimeHours, int cityTalkTimeMin, int cityTalkTimeSec)
@@ -70,7 +61,6 @@ namespace HW3Task2
                 CityTalkTimeMin = cityTalkTimeMin;
                 CityTalkTimeSec = cityTalkTimeSec;
             }
-
         }
 
         class Abonents
@@ -85,63 +75,67 @@ namespace HW3Task2
                 abonents.Add(newAbonent);
             }
            
-         
-
+            public void Show(Abonent ab)
+            {
+                Console.WriteLine($"{ab.LastName} {ab.FirstName} {ab.MidleName}");
+                Console.WriteLine($"ИНН->{ ab.IdNumber}--Карта->{ ab.CardNumber}--Дебет->{ ab.CardDebet}--Кредит->{ ab.CardCredit}");
+                Console.WriteLine($"Время междугородных разговоров->{ab.IntercityTalkTimeHours}:{ab.IntercityTalkTimeMin}:{ab.IntercityTalkTimeSec}");
+                Console.WriteLine($"Время городских разговоров->{ab.CityTalkTimeHours}:{ab.CityTalkTimeMin}:{ab.CityTalkTimeSec}");
+                Console.WriteLine();
+            }
             public void Print()
             {
                 foreach (var a in abonents)
                 {
-                    Console.WriteLine($"{a.LastName} {a.FirstName} {a.MidleName}");
-                    Console.WriteLine($"ИНН->{ a.IdNumber}--Карта->{ a.CardNumber}--Дебет->{ a.CardDebet}--Кредит->{ a.CardCredit}");
-                    Console.WriteLine($"Время междугородных разговоров->{a.IntercityTalkTimeHours}:{a.IntercityTalkTimeMin}:{a.IntercityTalkTimeSec}");
-                    Console.WriteLine($"Время городских разговоров->{a.CityTalkTimeHours}:{a.CityTalkTimeMin}:{a.CityTalkTimeSec}");
-                    Console.WriteLine();
+                    Show(a);             
                 }
-
             }
-            public void Print(int cityTalkTimeHours,
-             int cityTalkTimeMin,
-             int cityTalkTimeSec)
+            //Перевод в секунды
+            public int TimeInSeconds(int hours,int min,int sec)
             {
-                foreach (var ab in abonents)
+                int seconds = hours * 3600+min*60 + sec;
+                return seconds;
+            }
+            //Сравниваем и выводим абонентов превышевших время 
+            public void PrintCityTalk(int TalkTimeHours,int TalkTimeMin,int TalkTimeSec)
+            {
+             int seconds = TimeInSeconds(TalkTimeHours, TalkTimeMin, TalkTimeSec);
+                foreach (var abon in abonents)
                 {
-                    if (ab.CityTalkTimeHours > cityTalkTimeHours)
+                    if (TimeInSeconds(abon.CityTalkTimeHours,abon.CityTalkTimeMin,abon.CityTalkTimeMin)>seconds)
                     {
-                        Print();
-                    }
-                    else
+                        Show(abon);
+                    }                 
+                }
+            }
+
+            public void PrintIntercityTalk()
+            {
+                //Сортировка в алфавитном порядке
+                var result = from abonent in abonents
+                             orderby abonent.LastName
+                             select abonent;               
+                foreach (var ab in result)
+                {
+                    if (TimeInSeconds(ab.IntercityTalkTimeHours,ab.IntercityTalkTimeMin,ab.IntercityTalkTimeSec)!=0)
                     {
-                        if (ab.CityTalkTimeMin > cityTalkTimeMin)
-                        {
-                            Print();
-                        }
-                        else
-                        {
-                            if (ab.CityTalkTimeSec > cityTalkTimeSec)
-                            {
-                                Print();
-                            }
-                        }                      
-                    }
+                        Show(ab);
+                    }                 
                 }
             }
         }
-
-
         class Program
         {
             static void Main(string[] args)
             {
-
-
                 Abonent abonent1 = new Abonent(3476678863, "Федоров", "Александр", "Иванович", "Осенняя 10", 4546567666685786, 1000, 5000,
                     10, 10, 55, 1, 55, 32);
                 Abonent abonent2 = new Abonent(3476674463, "Сур", "Алексей", "Иванович", "Украинская 10", 6546567607685786, 100, 500,
                     16, 13, 48, 1, 20, 32);
                 Abonent abonent3 = new Abonent(3455678863, "Чернова", "Анна", "Петровна", "Мамона 167", 4544567606685786, 300, 7,
-                    34, 45, 55, 4, 25, 32);
+                    0, 0, 0, 4, 25, 32);
                 Abonent abonent4 = new Abonent(2476678860, "Алексеева", "Анастасия", "Викторовна", "Тихая 89", 6546567699685786, 10, 700,
-                    15, 42, 55, 6, 15, 32);
+                    0, 0, 0, 6, 15, 32);
 
                 Abonents abonents = new Abonents();
                 abonents.inputAbontData(abonent1);
@@ -156,9 +150,12 @@ namespace HW3Task2
                 int min = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Секунды-");
                 int sec = Convert.ToInt32(Console.ReadLine());
-                abonents.Print(hours, min, sec);
+                abonents.PrintCityTalk(hours, min, sec);
+                Console.WriteLine();
+                Console.WriteLine("Абоненты использовавшие междугороднюю связь:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                abonents.PrintIntercityTalk();
                 Console.ReadLine();
-
             }
         }
     }
