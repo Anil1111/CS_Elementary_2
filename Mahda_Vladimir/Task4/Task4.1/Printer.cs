e4c280313d5537fa845310eed7f928eb1f4f21b7
Vladimir Mahda <vladimir.mahda@gmail.com>
2018-10-14 14:23:17 +0300

    task4 added

---
+++ Mahda_Vladimir/Task4/Task4.1/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._1
+{
+    public class Printer
+    {
+        virtual public void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"This is {value}");
+        }
+    }
+}
---
+++ Mahda_Vladimir/Task4/Task4.4/Classes/ProDocumentWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._4.Classes
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт в версии Pro");
+        }
+
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
