2b73afe1e9045351a3b49f1083f51c760fe460ff
Yakimenko Igor <igor.jakimenko@privatbank.ua>
2018-10-23 21:33:49 +0300

    HomeWork4

---
+++ IgorYakimenko/HomeWork4/inheritance/Item3/Plane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item3
+{
+    class Plane : Vehicle
+    {
+        public double Height { get; set; }
+        public double NumberOfPassengers { get; set; }
+    }
+}
