3b9d6b11dc4b4dcba57eaf1c0c067950fcf5d8c7
HILLELTEACHERPC\User <polomkinanton@gmail.com>
2018-10-25 21:17:16 +0300

    Stratege example was added

---
+++ !Serhii_Artemenko/!Other/Design Patterns/Strategy/Examples/Strategy/Sorting/Strategy/Concrete/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting.Strategy.Concrete
+{
+    class BubbleSort : Strategy
+    {
+        public override void Sort(ref int[] array)
+        {
+            Console.WriteLine("BubbleSort");
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                    if (array[j] < array[j - 1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+            }
+        }
+    }
+}
--- !Serhii_Artemenko/007_IO/Examples/002 - Microsoft Input Output/001 - Navigation/FileSystemWatcherDemo/Program.cs
+++ !Serhii_Artemenko/007_IO/Examples/002 - Microsoft Input Output/001 - Navigation/FileSystemWatcherDemo/Program.cs
@@ -13,10 +13,13 @@ namespace FileSystemWatcherDemo
             // Создание наблюдателя и сосредоточение его внимания на системном диске.
             var watcher = new FileSystemWatcher { Path = @"c:\bin" };
 
+            
+
             // Зарегистрировать обработчики событий.
-            watcher.Created += new FileSystemEventHandler(WatcherChanged);
-            watcher.Deleted += WatcherChanged;
+            watcher.Created += new FileSystemEventHandler(WatcherCreated);
+            watcher.Deleted += new FileSystemEventHandler(WatcherDeleted);
             watcher.Renamed += Watcher_Renamed;
+            watcher.Error += Watcher_Error;
 
             // Начать мониторинг.
             watcher.EnableRaisingEvents = true;
@@ -28,15 +31,25 @@ namespace FileSystemWatcherDemo
             Console.ReadLine();
         }
 
+        private static void Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine("Directory error");
+        }
+
         private static void Watcher_Renamed(object sender, RenamedEventArgs e)
         {
             Console.WriteLine("Directory renamed({0}): {1}", e.ChangeType, e.FullPath);
         }
 
         // Обработчик события.
-        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        static void WatcherDeleted(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine("Directory changed({0}): {1}", e.ChangeType, e.FullPath);
+            Console.WriteLine("Directory Deleted({0}): {1}", e.ChangeType, e.FullPath);
+        }
+
+        static void WatcherCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Directory Created({0}): {1}", e.ChangeType, e.FullPath);
         }
     }
 }
