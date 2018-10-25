70beb50302378595f70543a67463a27229f2c8e8
streetsata <streetsata@live.ru>
2018-10-25 14:41:49 +0300

    Strategy was added

---
+++ !Serhii_Artemenko/!Other/Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,13 @@
+for (int i = 0; i < array.Length - 1; i++)
+{
+    int k = i;
+    for (int j = i + 1; j < array.Length; j++)
+        if (array[k] > array[j])
+            k = j;
+    if (k != i)
+    {
+        int temp = array[k];
+        array[k] = array[i];
+        array[i] = temp;
+    }
+}
