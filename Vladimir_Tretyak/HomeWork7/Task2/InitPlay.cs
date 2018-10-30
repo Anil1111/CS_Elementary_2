161ba0203ed070ad87cfadf4a5712c445cd57c98
Vladimir Tretyak <vladitretyak@i.ua>
2018-10-30 15:39:00 +0200

    HW7T2

---
+++ Vladimir_Tretyak/HomeWork7/Task2/InitPlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace Player
+{
+    static class InitPlay
+    {
+        public static  WindowsMediaPlayer Wplayer;  
+           
+        public static void  initPlay()
+        {
+            WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+            Wplayer = wplayer;
+        }
+    }
+}
