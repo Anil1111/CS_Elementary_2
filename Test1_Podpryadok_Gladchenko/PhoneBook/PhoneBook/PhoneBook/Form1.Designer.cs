a3fabda8eb1cd9de6820ddd816c8558cf926d86a
Podpryadok_Igor <podpryadok@gmail.com>
2018-11-01 20:02:00 +0200

    add all not functionl task)

---
+++ Test1_Podpryadok_Gladchenko/PhoneBook/PhoneBook/PhoneBook/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowBox = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.skypeBox = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShowBox
+            // 
+            this.ShowBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowBox.Location = new System.Drawing.Point(13, 13);
+            this.ShowBox.Multiline = true;
+            this.ShowBox.Name = "ShowBox";
+            this.ShowBox.Size = new System.Drawing.Size(444, 341);
+            this.ShowBox.TabIndex = 0;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_add.Location = new System.Drawing.Point(463, 13);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(82, 23);
+            this.btn_add.TabIndex = 1;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameBox.Enabled = false;
+            this.nameBox.Location = new System.Drawing.Point(464, 43);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(249, 20);
+            this.nameBox.TabIndex = 2;
+            this.nameBox.Text = "Name";
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.phoneBox.Enabled = false;
+            this.phoneBox.Location = new System.Drawing.Point(463, 69);
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(250, 20);
+            this.phoneBox.TabIndex = 3;
+            this.phoneBox.Text = "Phone";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.emailBox.Enabled = false;
+            this.emailBox.Location = new System.Drawing.Point(463, 95);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(250, 20);
+            this.emailBox.TabIndex = 4;
+            this.emailBox.Text = "Email";
+            // 
+            // skypeBox
+            // 
+            this.skypeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.skypeBox.Enabled = false;
+            this.skypeBox.Location = new System.Drawing.Point(463, 121);
+            this.skypeBox.Name = "skypeBox";
+            this.skypeBox.Size = new System.Drawing.Size(250, 20);
+            this.skypeBox.TabIndex = 5;
+            this.skypeBox.Text = "Skype";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_save.Enabled = false;
+            this.btn_save.Location = new System.Drawing.Point(638, 14);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 6;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchBox.Location = new System.Drawing.Point(463, 176);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(249, 20);
+            this.SearchBox.TabIndex = 7;
+            this.SearchBox.Text = "Search";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_search.DialogResult = System.Windows.Forms.DialogResult.Retry;
+            this.btn_search.Location = new System.Drawing.Point(463, 147);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(250, 23);
+            this.btn_search.TabIndex = 8;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_update.Enabled = false;
+            this.btn_update.Location = new System.Drawing.Point(551, 14);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(81, 23);
+            this.btn_update.TabIndex = 9;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 361);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.skypeBox);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.ShowBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ShowBox;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox skypeBox;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_update;
+    }
+}
+
---
+++ Test1_Podpryadok_Gladchenko/PhoneBook/PhoneBook/PhoneBook/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            btn_add.Enabled = false;
+            btn_save.Enabled = true;
+            nameBox.Enabled = true;
+            phoneBox.Enabled = true;
+            emailBox.Enabled = true;
+            skypeBox.Enabled = true;
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            btn_add.Enabled = true;
+            btn_save.Enabled = false;
+            nameBox.Enabled = false;
+            phoneBox.Enabled = false;
+            emailBox.Enabled = false;
+            skypeBox.Enabled = false;
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            btn_add.Enabled = false;
+            btn_update.Enabled = true;
+            nameBox.Enabled = true;
+            phoneBox.Enabled = true;
+            emailBox.Enabled = true;
+            skypeBox.Enabled = true;
+        }
+    }
+}
