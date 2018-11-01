b62b4b82614a768c5dd11d3f765f6b336ddf2a93
Vladimir Mahda <vladimir.mahda@gmail.com>
2018-11-01 16:43:32 +0200

    task8_Delegates added

---
+++ Mahda_Vladimir/Task8_Delegates/Task8.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента
+//и возвращает среднее арифметическое этих аргументов.
+
+namespace Task8._1
+{
+    public delegate int DelegateIntAverage(int number1, int number2, int number3);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateIntAverage myDelegate = delegate (int number1, int number2, int number3) { return (number1 + number2 + number3) / 3; };
+            Console.WriteLine($"Average: {myDelegate(3, 6, 12)}" );
+            Console.ReadKey();
+        }
+    }
+}
