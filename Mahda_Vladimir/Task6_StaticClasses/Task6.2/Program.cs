753f57bbe70e263f8cc17f4e6a346823216394b7
Vladimir Mahda <vladimir.mahda@gmail.com>
2018-11-04 02:16:08 +0200

    task 6_StaticClasses added

---
+++ Mahda_Vladimir/Task6_StaticClasses/Task6.2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+// Создать статический класс FindAndReplaceManager с методом void FindNext(string str) 
+//для поиска по книге.При вызове этого метода, производится последовательный поиск 
+//строки в книге. (Книга любая, подгружаем с помощью Filestream)
+
+namespace Task6._2
+{
+    public static class FindAndReplaceManager
+    {
+        public static bool FindNext(string page, string str, int i)
+        {
+            bool result = page.Contains(str);
+            if (result)
+            {
+                Console.WriteLine($"The substring {str} is present on page {i}");
+                Console.WriteLine($"{str} begins at character position {page.IndexOf(str)}");
+            }
+            return result;
+        }
+
+        public static string[] OpenPDFFile(string path)
+        {
+            try
+            {
+                using (PdfReader reader = new PdfReader(path))
+                {
+                    string[] page = new string[reader.NumberOfPages + 1];
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        //парсим книгу постранично
+                        page[i] = PdfTextExtractor.GetTextFromPage(reader, i);
+                    }
+                    return page;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The file could not be read\n" + ex.Message);
+                return new string[] {};
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //строка для поиска
+            string findString = "привет";
+            //флаг совпадений
+            bool contains = false;
+            //создаем массив страниц
+            string[] text = FindAndReplaceManager.OpenPDFFile("book.pdf");
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (FindAndReplaceManager.FindNext(text[i], findString, i))
+                    contains=true;
+            }
+            //Если не было совпадений, то сообщаем об этом
+            if (!contains && text.Length != 0)
+                Console.WriteLine("There were no matches");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Mahda_Vladimir/Task6_StaticClasses/Task6.4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+//Изучить Singelton.Создать класс, где необходимо подключить базу данных в проект и класс, 
+//который будет отвечать за соединение с ней.Один раз создается соединение 
+//и нет нужны создавать его снова и снова.
+
+namespace Task6._4
+{
+    public sealed class MyDBConnection
+    {
+        private static MySqlConnection instance;
+        private const string connectionString = "Server=127.0.0.1;" +
+                                                "Database=InternetShop;" +
+                                                "User id=root;" +
+                                                "Password=";
+        private MyDBConnection() { }
+
+        public static MySqlConnection Instance
+        {
+            get     //This is Singleton!!!
+            {
+                if (instance == null)
+                    instance = new MySqlConnection(connectionString);
+                return instance;
+            }
+        }
+
+        public static void QueryInternetShopAllProducts(MySqlConnection connection)
+        {
+            string sql = "SELECT * FROM InternetShop.products;";
+
+            // Create command
+            MySqlCommand cmd = new MySqlCommand
+            {
+                // Set connection for command
+                Connection = connection,
+                CommandText = sql
+            };
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        // Get article
+                        int article = Convert.ToInt32(reader.GetValue(0)); // 0
+                        // Get brandId
+                        int brandId = Convert.ToInt32(reader.GetValue(1)); // 1
+                        // Get typeId
+                        int typeId = Convert.ToInt32(reader.GetValue(2)); // 2
+                        // Get categoryId
+                        int categoryId = Convert.ToInt32(reader.GetValue(3)); // 3
+                        // Get price
+                        double price = Convert.ToDouble(reader.GetValue(4)); // 4
+
+                        Console.WriteLine("--------------------");
+                        Console.WriteLine($"article:    {article}");
+                        Console.WriteLine($"brandId:    {brandId}");
+                        Console.WriteLine($"typeId:     {typeId}");
+                        Console.WriteLine($"categoryId: {categoryId}");
+                        Console.WriteLine($"price:      {price}");
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection connection = MyDBConnection.Instance;
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                connection.Open();
+
+                Console.WriteLine("Connection successful!");
+                MyDBConnection.QueryInternetShopAllProducts(connection);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Connection close!");
+                // Close connection.
+                connection.Close();
+                // Dispose object, Freeing Resources.
+                connection.Dispose();
+            }
+            Console.Read();
+        }
+    }
+}
