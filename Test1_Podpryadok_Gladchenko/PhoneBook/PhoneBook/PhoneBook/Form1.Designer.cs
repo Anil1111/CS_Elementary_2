8114305bf75d3c83a284fa4ffc6e2f8ad36473bf
Podpryadok_Igor <podpryadok@gmail.com>
2018-11-08 19:24:52 +0200

    Add new not work Task

--- Test1_Podpryadok_Gladchenko/PhoneBook/PhoneBook/PhoneBook/Form1.Designer.cs
+++ Test1_Podpryadok_Gladchenko/PhoneBook/PhoneBook/PhoneBook/Form1.Designer.cs
@@ -38,6 +38,11 @@
             this.btn_search = new System.Windows.Forms.Button();
             this.btn_delate = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_phone = new System.Windows.Forms.Label();
+            this.label_email = new System.Windows.Forms.Label();
+            this.label_skype = new System.Windows.Forms.Label();
+            this.label_search = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,43 +59,40 @@
             // 
             // nameBox
             // 
+            this.nameBox.AccessibleDescription = "";
             this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.nameBox.Enabled = false;
-            this.nameBox.Location = new System.Drawing.Point(464, 43);
+            this.nameBox.Location = new System.Drawing.Point(463, 55);
             this.nameBox.Name = "nameBox";
             this.nameBox.Size = new System.Drawing.Size(249, 20);
             this.nameBox.TabIndex = 2;
-            this.nameBox.Text = "Name";
             // 
             // phoneBox
             // 
             this.phoneBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.phoneBox.Enabled = false;
-            this.phoneBox.Location = new System.Drawing.Point(463, 69);
+            this.phoneBox.Location = new System.Drawing.Point(463, 94);
             this.phoneBox.Name = "phoneBox";
             this.phoneBox.Size = new System.Drawing.Size(250, 20);
             this.phoneBox.TabIndex = 3;
-            this.phoneBox.Text = "Phone";
             // 
             // emailBox
             // 
             this.emailBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.emailBox.Enabled = false;
-            this.emailBox.Location = new System.Drawing.Point(463, 95);
+            this.emailBox.Location = new System.Drawing.Point(463, 133);
             this.emailBox.Name = "emailBox";
             this.emailBox.Size = new System.Drawing.Size(250, 20);
             this.emailBox.TabIndex = 4;
-            this.emailBox.Text = "Email";
             // 
             // skypeBox
             // 
             this.skypeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.skypeBox.Enabled = false;
-            this.skypeBox.Location = new System.Drawing.Point(463, 121);
+            this.skypeBox.Location = new System.Drawing.Point(463, 172);
             this.skypeBox.Name = "skypeBox";
             this.skypeBox.Size = new System.Drawing.Size(250, 20);
             this.skypeBox.TabIndex = 5;
-            this.skypeBox.Text = "Skype";
             // 
             // btn_save
             // 
@@ -107,17 +109,16 @@
             // SearchBox
             // 
             this.SearchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.SearchBox.Location = new System.Drawing.Point(463, 176);
+            this.SearchBox.Location = new System.Drawing.Point(462, 300);
             this.SearchBox.Name = "SearchBox";
             this.SearchBox.Size = new System.Drawing.Size(249, 20);
             this.SearchBox.TabIndex = 7;
-            this.SearchBox.Text = "Search";
             // 
             // btn_search
             // 
             this.btn_search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_search.DialogResult = System.Windows.Forms.DialogResult.Retry;
-            this.btn_search.Location = new System.Drawing.Point(463, 202);
+            this.btn_search.Location = new System.Drawing.Point(462, 326);
             this.btn_search.Name = "btn_search";
             this.btn_search.Size = new System.Drawing.Size(250, 23);
             this.btn_search.TabIndex = 8;
@@ -146,11 +147,66 @@
             this.dataGridView.TabIndex = 10;
             this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
             // 
+            // label_name
+            // 
+            this.label_name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(463, 39);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(35, 13);
+            this.label_name.TabIndex = 11;
+            this.label_name.Text = "Name";
+            // 
+            // label_phone
+            // 
+            this.label_phone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_phone.AutoSize = true;
+            this.label_phone.Location = new System.Drawing.Point(463, 78);
+            this.label_phone.Name = "label_phone";
+            this.label_phone.Size = new System.Drawing.Size(38, 13);
+            this.label_phone.TabIndex = 12;
+            this.label_phone.Text = "Phone";
+            // 
+            // label_email
+            // 
+            this.label_email.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_email.AutoSize = true;
+            this.label_email.Location = new System.Drawing.Point(463, 117);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(32, 13);
+            this.label_email.TabIndex = 13;
+            this.label_email.Text = "Email";
+            // 
+            // label_skype
+            // 
+            this.label_skype.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_skype.AutoSize = true;
+            this.label_skype.Location = new System.Drawing.Point(463, 156);
+            this.label_skype.Name = "label_skype";
+            this.label_skype.Size = new System.Drawing.Size(37, 13);
+            this.label_skype.TabIndex = 14;
+            this.label_skype.Text = "Skype";
+            // 
+            // label_search
+            // 
+            this.label_search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_search.AutoSize = true;
+            this.label_search.Location = new System.Drawing.Point(463, 284);
+            this.label_search.Name = "label_search";
+            this.label_search.Size = new System.Drawing.Size(41, 13);
+            this.label_search.TabIndex = 15;
+            this.label_search.Text = "Search";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(721, 361);
+            this.Controls.Add(this.label_search);
+            this.Controls.Add(this.label_skype);
+            this.Controls.Add(this.label_email);
+            this.Controls.Add(this.label_phone);
+            this.Controls.Add(this.label_name);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.btn_delate);
             this.Controls.Add(this.btn_search);
@@ -180,6 +236,11 @@
         private System.Windows.Forms.Button btn_search;
         private System.Windows.Forms.Button btn_delate;
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_phone;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.Label label_skype;
+        private System.Windows.Forms.Label label_search;
     }
 }
 
