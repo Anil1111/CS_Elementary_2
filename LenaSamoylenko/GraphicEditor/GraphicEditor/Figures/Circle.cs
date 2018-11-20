90f0b26d66455ea40a2e6e474d3bc45d4eab2c6a
Lena Samoyenko <aknelyomas@gmail.com>
2018-11-20 16:35:54 +0200

    Editor in procces

---
+++ LenaSamoylenko/GraphicEditor/GraphicEditor/Figures/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphicEditor.Figures
+{
+    internal class Circle:Figure
+    {
+
+        public Circle(PictureBox pb):base(pb)
+        {
+
+        }
+
+        public override void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            myBitmap = new Bitmap(this.pictureBox.Width, this.pictureBox.Height);
+            var fig = Graphics.FromImage(myBitmap);
+
+            Pen p = new Pen(Color.Green);
+            fig.DrawRectangle(p, a, b, e.X - a, e.Y - b);
+            pictureBox.Image = myBitmap;
+
+        }
+    }
+
+    
+}
---
+++ LenaSamoylenko/GraphicEditor/GraphicEditor/Figures/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GraphicEditor.Figures
+{
+    internal class Rectangle : Figure
+    {
+        public Rectangle(PictureBox pb) : base(pb)
+        {
+        }
+
+        public override void pb_MouseUp(object sender, MouseEventArgs e)
+        {
+            myBitmap = new Bitmap(this.pictureBox.Width, this.pictureBox.Height);
+            var fig = Graphics.FromImage(myBitmap);
+
+            Pen p = new Pen(Color.Green);
+            fig.DrawRectangle(p, a, b, e.X - a, e.Y - b);
+            pictureBox.Image = myBitmap;
+
+        }
+    }
+}
