91d60e74d80c4edb07951373b439cc37ed10db60
streetsata <streetsata@live.ru>
2018-11-22 14:01:49 +0200

    Thread was added

---
+++ !Serhii_Artemenko/012_Multithreading and Multitasking/Examples/001_Review/001_Review/001_MonitoringLib/Recorder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+using static System.Console;
+using static System.Diagnostics.Process;
+
+namespace _001_MonitoringLib
+{
+    public class Recorder
+    {
+        static Stopwatch timer = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+
+        public static void Start()
+        {
+            // Вся выделенная в настоящий момент память
+            // будет собрана до записи количества использованной памяти.
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            bytesPhysicalBefore = GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+        }
+
+        public static void Stop()
+        {
+            timer.Stop();
+
+            long bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+
+            WriteLine("Stoped Recording");
+            WriteLine($"{bytesPhysicalAfter - bytesPhysicalBefore:N0} physical bytes used");
+            WriteLine($"{bytesVirtualAfter - bytesVirtualBefore:N0} physical bytes used");
+
+            WriteLine($"{timer.Elapsed} time span ellapsed");
+            WriteLine($"{timer.ElapsedMilliseconds} total milliseconds ellapsed");
+
+        }
+    }    
+}
