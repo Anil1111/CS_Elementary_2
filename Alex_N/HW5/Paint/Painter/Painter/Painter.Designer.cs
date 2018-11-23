fc06a41c077b125303becdf9ff66ac0a6c380c7c
Alex Nevon <lexaiiimejibnevon@gmail.com>
2018-11-23 02:53:24 +0200

    hw pietro five sinco 5

---
+++ Alex_N/HW5/Paint/Painter/Painter/Painter.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Painter
+{
+    partial class Painter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cDialog = new System.Windows.Forms.ColorDialog();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.bSizeBar = new System.Windows.Forms.TrackBar();
+            this.sizeIndicator = new System.Windows.Forms.Label();
+            this.FillCheckBox = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.ellipsButton = new System.Windows.Forms.Button();
+            this.recBtn = new System.Windows.Forms.Button();
+            this.paper = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bSizeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cDialog
+            // 
+            this.cDialog.FullOpen = true;
+            // 
+            // colorButton
+            // 
+            this.colorButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.colorButton.BackColor = System.Drawing.Color.Black;
+            this.colorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.colorButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.colorButton.FlatAppearance.BorderSize = 0;
+            this.colorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.colorButton.Location = new System.Drawing.Point(300, 495);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(50, 50);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.UseVisualStyleBackColor = false;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            this.colorButton.MouseLeave += new System.EventHandler(this.colorButton_MouseLeave);
+            this.colorButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.colorButton_MouseMove);
+            // 
+            // bSizeBar
+            // 
+            this.bSizeBar.AutoSize = false;
+            this.bSizeBar.BackColor = System.Drawing.Color.White;
+            this.bSizeBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bSizeBar.Location = new System.Drawing.Point(43, 495);
+            this.bSizeBar.Maximum = 250;
+            this.bSizeBar.Minimum = 1;
+            this.bSizeBar.Name = "bSizeBar";
+            this.bSizeBar.Size = new System.Drawing.Size(214, 49);
+            this.bSizeBar.TabIndex = 2;
+            this.bSizeBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.bSizeBar.Value = 1;
+            this.bSizeBar.Scroll += new System.EventHandler(this.bSizeBar_Scroll);
+            // 
+            // sizeIndicator
+            // 
+            this.sizeIndicator.AutoSize = true;
+            this.sizeIndicator.BackColor = System.Drawing.Color.White;
+            this.sizeIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sizeIndicator.Location = new System.Drawing.Point(138, 516);
+            this.sizeIndicator.Name = "sizeIndicator";
+            this.sizeIndicator.Size = new System.Drawing.Size(17, 17);
+            this.sizeIndicator.TabIndex = 5;
+            this.sizeIndicator.Text = "1\r\n";
+            this.sizeIndicator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FillCheckBox
+            // 
+            this.FillCheckBox.AutoSize = true;
+            this.FillCheckBox.BackColor = System.Drawing.Color.White;
+            this.FillCheckBox.Font = new System.Drawing.Font("Myriad Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FillCheckBox.Location = new System.Drawing.Point(487, 512);
+            this.FillCheckBox.Name = "FillCheckBox";
+            this.FillCheckBox.Size = new System.Drawing.Size(95, 24);
+            this.FillCheckBox.TabIndex = 6;
+            this.FillCheckBox.Text = "Fill Figure";
+            this.FillCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.saveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.saveBtn.BackgroundImage = global::Painter.Properties.Resources.save_ico;
+            this.saveBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.saveBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveBtn.FlatAppearance.BorderSize = 0;
+            this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveBtn.Location = new System.Drawing.Point(764, 494);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(50, 50);
+            this.saveBtn.TabIndex = 7;
+            this.saveBtn.UseVisualStyleBackColor = false;
+            this.saveBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.saveBtn_MouseClick);
+            // 
+            // ellipsButton
+            // 
+            this.ellipsButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ellipsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ellipsButton.BackgroundImage = global::Painter.Properties.Resources.circle_ico;
+            this.ellipsButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ellipsButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ellipsButton.FlatAppearance.BorderSize = 0;
+            this.ellipsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ellipsButton.Location = new System.Drawing.Point(412, 495);
+            this.ellipsButton.Name = "ellipsButton";
+            this.ellipsButton.Size = new System.Drawing.Size(50, 50);
+            this.ellipsButton.TabIndex = 4;
+            this.ellipsButton.UseVisualStyleBackColor = false;
+            this.ellipsButton.Click += new System.EventHandler(this.ellipsButton_Click);
+            // 
+            // recBtn
+            // 
+            this.recBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.recBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.recBtn.BackgroundImage = global::Painter.Properties.Resources.rect_ico;
+            this.recBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.recBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.recBtn.FlatAppearance.BorderSize = 0;
+            this.recBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.recBtn.Location = new System.Drawing.Point(356, 495);
+            this.recBtn.Name = "recBtn";
+            this.recBtn.Size = new System.Drawing.Size(50, 50);
+            this.recBtn.TabIndex = 3;
+            this.recBtn.UseVisualStyleBackColor = false;
+            this.recBtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.recBtn_MouseClick);
+            // 
+            // paper
+            // 
+            this.paper.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.paper.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.paper.Location = new System.Drawing.Point(0, 0);
+            this.paper.Name = "paper";
+            this.paper.Size = new System.Drawing.Size(826, 556);
+            this.paper.TabIndex = 0;
+            this.paper.TabStop = false;
+            this.paper.DoubleClick += new System.EventHandler(this.paper_DoubleClick);
+            this.paper.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paper_MouseDown);
+            this.paper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paper_MouseMove);
+            this.paper.MouseUp += new System.Windows.Forms.MouseEventHandler(this.paper_MouseUp);
+            // 
+            // Painter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(826, 556);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.FillCheckBox);
+            this.Controls.Add(this.sizeIndicator);
+            this.Controls.Add(this.ellipsButton);
+            this.Controls.Add(this.recBtn);
+            this.Controls.Add(this.bSizeBar);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.paper);
+            this.Name = "Painter";
+            this.Text = "Painter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bSizeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paper)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox paper;
+        private System.Windows.Forms.ColorDialog cDialog;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.TrackBar bSizeBar;
+        private System.Windows.Forms.Button recBtn;
+        private System.Windows.Forms.Button ellipsButton;
+        private System.Windows.Forms.Label sizeIndicator;
+        private System.Windows.Forms.CheckBox FillCheckBox;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Alex_N/HW5/Paint/Painter/Painter/Painter.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Painter
+{
+    public partial class Painter : Form
+    {
+        Color crColor;//цвет
+        Pen upen;//руча
+        Graphics canvas;//Загадочная хрень. Плохо изученная. Ну.. В мсдн написано что надо это юзать. Значит будем юзать
+        Point crMousePosition;//текущий поинт курсора(Экран)
+        Point prMousePosition;//Прошедший поинт курсора(Тоже Экран)
+        Bitmap art;//Битмапа-Рисовака 
+        SolidBrush brushShape;//Браша - Это для филла. Ну так в МСДН НАПИСАНО, я че. я ниче. 
+        Size brushSize;//Фак вот это не помню..... А - это типа переменная САЙйз в которую я пхнул Со скрола ВалуЭ.
+        int mainTool = 0;//основная тула 
+        int additionalTool = 0;//Адитиональ Тулла!!
+
+        public Painter()//стандартная подлива
+        {
+            InitializeComponent();
+        }
+
+
+        private void AllToolsOFF()//ТулоОбнуляшка 
+        {
+
+            mainTool = 0;
+            additionalTool = 0;
+
+        }
+
+        private Rectangle FigureDrawCalculator(int x1, int y1, int x2, int y2)// формирователь правильного ректангуляра относительно стартовой и конечной точки курсора
+        {
+            Size figureSizeTemp = new Size(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+
+            if (x1 > x2 & y1 > y2)
+            {
+                return new Rectangle(prMousePosition, figureSizeTemp);
+            }
+            if (x1 < x2 & y1 < y2)
+            {
+                return new Rectangle(crMousePosition, figureSizeTemp);
+            }
+            if (x1 > x2 & y1 < y2)
+            {
+                return new Rectangle(new Point(x2, y1), figureSizeTemp);
+            }
+            if (x1 < x2 & y1 > y2)
+            {
+                return new Rectangle(new Point(x1, y2), figureSizeTemp);
+            }
+            else
+            {
+                return new Rectangle(new Point(0, 0), new Size(0, 0));
+            }
+
+        }
+
+
+        private void Drawing()
+        {
+            brushShape.Color = crColor;//СОЛИД БРАША!!!  
+            upen.Color = crColor;//ручка... при отрисовке DrawRectangle returs pretty interesting result.  
+            switch (additionalTool)
+
+            {
+                case 0://проверка инструментов + Если филл включен 
+                    canvas.FillEllipse(brushShape, new Rectangle(new Point(crMousePosition.X - (bSizeBar.Value / 2), crMousePosition.Y - (bSizeBar.Value / 2)), brushSize));
+                    break;
+                case 1:
+                    //
+                    if (FillCheckBox.Checked)
+                    {
+                        canvas.FillRectangle(brushShape, FigureDrawCalculator(crMousePosition.X, crMousePosition.Y, prMousePosition.X, prMousePosition.Y));
+                    }
+                    else
+                    {
+                        
+                        canvas.DrawRectangle(upen, FigureDrawCalculator(crMousePosition.X, crMousePosition.Y, prMousePosition.X, prMousePosition.Y));
+                    }
+
+                    break;
+                case 2:
+                    if (FillCheckBox.Checked)
+                    {
+                        canvas.FillEllipse(brushShape, FigureDrawCalculator(crMousePosition.X, crMousePosition.Y, prMousePosition.X, prMousePosition.Y));
+                    }
+                    else
+                    {
+
+                        canvas.DrawEllipse(upen, FigureDrawCalculator(crMousePosition.X, crMousePosition.Y, prMousePosition.X, prMousePosition.Y));
+                    }                    
+                    break;
+                default:
+                    break;
+            }
+
+            //canvas.FillRectangle(brushShape, new RectangleF(new Point(crMousePosition.X - (bSizeBar.Value/2), crMousePosition.Y - (bSizeBar.Value/2)), brushSize));//Ректангль Схапе!!!!...
+
+            paper.Image = art;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            brushSize = new Size(bSizeBar.Value, bSizeBar.Value);//Принимает значения ползунка размеров
+            brushShape = new SolidBrush(crColor);//Инициализируем солид брашу 
+            art = new Bitmap(paper.Width, paper.Height);//создаем битмапу по размеру нашего канваса. полотна кароч. 
+            crMousePosition = Cursor.Position;//писваиваем поинту текущее положение мышки(НА ЭКРАНЕ !!! не на форме)
+            crColor = colorButton.BackColor;//приваиваем переменной текущего цвета цвет кнопки выбора цветов. цвет цвет цвет маслечко масляное 
+
+            upen = new Pen(crColor,bSizeBar.Value);// инициализируем ручку как новую с шейпой Солид Браши????? тут вопрос конечно. да тут вопрос. 
+            upen.DashPattern = new float[] { 4.0F, 2.0F, 1.0F, 3.0F };
+            upen.DashCap = System.Drawing.Drawing2D.DashCap.Round;//какой будет конец у нашей ручки я так понял.. МСДН пфффффффф. мсдн. Скругляшки 
+            upen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;//тип линии - Солид канешно штрихпунктир выглядит норм. но чет сплошняком лучше 
+
+            canvas = Graphics.FromImage(art); //график у нас будет работать с битмапой
+            canvas.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;//ну тут такое тоже м МСДН ПОДСМОТРЕЛЬ. ну такое. такое. Типа сглаживание..
+
+            saveFileDialog.FileName = "MyCreation";//Шаблон названия файла 
+            saveFileDialog.Filter = "PNG(*.PNG; *.png)| *.PNG; *.png; | Jpeg(*.JPEG; *.jpeg) | *.JPEG; *.jpeg; | Bitmap(*.BMP; *.bmp)|*.BMP; *.bmp |All files(*.*) | *.*";//ШАблон форматов файлов
+
+
+        }
+
+
+
+        private void paper_MouseDown(object sender, MouseEventArgs e)//Нажимашка на Бамажка 
+        {
+            brushSize.Width = bSizeBar.Value;
+            brushSize.Height = bSizeBar.Value;
+            upen.Width = bSizeBar.Value;
+            mainTool = 1;
+            if (additionalTool != 0)
+            {
+                prMousePosition = PointToClient(Cursor.Position);
+            }
+
+        }
+
+        private void paper_MouseUp(object sender, MouseEventArgs e)
+        {
+            mainTool = 0;
+
+            if (additionalTool != 0)
+            {
+                Drawing();
+            }
+
+            upen.Brush.Dispose();//А в мсднЕ нАписАно, что ЭЭЭтоО очищААет память. И карму.  
+
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            cDialog.ShowDialog();
+            crColor = cDialog.Color;
+            colorButton.BackColor = cDialog.Color;
+        }
+
+        private void colorButton_MouseLeave(object sender, EventArgs e)
+        {
+            colorButton.Width = 50;
+            colorButton.Height = 50;
+            colorButton.Location = new Point(300, 495);
+        }
+
+        private void colorButton_MouseMove(object sender, MouseEventArgs e)
+        {
+            colorButton.Width = 60;
+            colorButton.Height = 60;
+            colorButton.Location = new Point(295, 490);
+        }
+
+        private void paper_MouseMove(object sender, MouseEventArgs e)//ОООООООООО ведем по канвасу мышой 
+        {
+
+
+            if (mainTool != 0 & additionalTool == 0)// Проверяем чо происходит. Если тулсы активированы какие то - Действуем.
+            {
+                prMousePosition = crMousePosition;
+                crMousePosition = PointToClient(Cursor.Position);
+                Drawing();
+            }
+            else
+            {
+                switch (additionalTool)
+                {
+                    case 1:
+                        crMousePosition = PointToClient(Cursor.Position);//Позицьйон Относительно формъйон. 
+                        break;
+                    case 2:
+                        crMousePosition = PointToClient(Cursor.Position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void recBtn_MouseClick(object sender, MouseEventArgs e)//клик по ректангль батон
+        {
+            additionalTool = 1;
+        }
+
+        private void paper_DoubleClick(object sender, EventArgs e)//ДАБЛ КЛИН НА ПОЛЕ - ОТМЕНА ВСЕХ ТУЛЗОВ 
+        {
+            AllToolsOFF();
+        }
+
+        private void ellipsButton_Click(object sender, EventArgs e)//клик по элипс-батОну 
+        {
+            additionalTool = 2;
+        }
+
+        private void bSizeBar_Scroll(object sender, EventArgs e)//размеры, тааааак это не интересно...
+        {
+            sizeIndicator.Text = bSizeBar.Value.ToString();
+        }
+
+        private void saveBtn_MouseClick(object sender, MouseEventArgs e)//О, О.....Сохраняшка....МММММММ______ :)
+        {          
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                art.Save(saveFileDialog.FileName);
+            }
+        }
+
+        //Фак. Я забыл стирачку сделать. Ну пох лан. я и контролЗет забыл и заливку и кривые . ой ну блин. .... 
+
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
